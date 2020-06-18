2e61776057bb24a72e91de74800029edfd1b920b
USUARIO <oscarcortina12@gmail.com>
2020-06-18 11:02:18 -0500

    Se ajusto la actividad inicial de la aplicación

--- app/src/main/java/com/example/turistmap/Routes/Routes.java
+++ app/src/main/java/com/example/turistmap/Routes/Routes.java
@@ -1,7 +1,7 @@
 package com.example.turistmap.Routes;
 
 public class Routes {
-    public static String server = "http://192.168.1.6:8000/";
+    public static String server = "http://192.168.0.10:8000/";
     public static String listar_eventos = server+"listarEventos";
     public static String listar_sitios = server+"listarSitios";
     public static String listar_actividades = server+"listarActividades";
